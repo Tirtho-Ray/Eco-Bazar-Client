73fd7548e55e1ac6d9e3b220d596ee5589e4ea1f
Tirtho-Ray <tirthoray10@gmail.com>
2025-09-13 20:02:13 +0600

    update logic

--- src/api/apiServices/productServices/productServices.js
+++ src/api/apiServices/productServices/productServices.js
@@ -1,6 +1,6 @@
 import { axiosInstance } from "../../axiosInstance/axiosInstance";
 
 export const fetchProduct  = async ()=>{
-    const data  =await axiosInstance.get("/product.json");
+    const {data}  =await axiosInstance.get("/product.json");
     return data;
 };
