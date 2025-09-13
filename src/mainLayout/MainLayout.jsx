7a1b4428d009c3604247677061e55d1368192a5e
Tirtho-Ray <tirthoray10@gmail.com>
2025-09-13 20:05:40 +0600

    add new kewWord

--- src/mainLayout/MainLayout.jsx
+++ src/mainLayout/MainLayout.jsx
@@ -2,13 +2,22 @@ import React from 'react';
 import Navbar from '../components/navbar/Navbar';
 import { Outlet } from 'react-router';
 import Footer from '../components/footer/Footer';
+import { Container } from '../components/ui/reUsable/Container';
+import Header from '../components/header/Header';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const MainLayout = () => {
+    const queryClient   =  new QueryClient()
     return (
         <div>
-            <Navbar />
-            <Outlet />
-            <Footer />
+            <QueryClientProvider client={queryClient}>
+             <Header />
+             <Container >
+               <Navbar />
+             </Container>
+             <Outlet />
+             <Footer />
+            </QueryClientProvider>
         </div>
     );
 };
