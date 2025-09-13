94ea52cd575b1272327dccb5d665cd99bda40007
Tirtho-Ray <tirthoray10@gmail.com>
2025-09-13 12:36:05 +0600

    write code for fetch product

---
+++ src/api/apiServices/productServices/productServices.js
@@ -0,0 +1,6 @@
+import { axiosInstance } from "../../axiosInstance/axiosInstance";
+
+export const fetchProduct  = async ()=>{
+    const data  =await axiosInstance.get("/product.json");
+    return data;
+};
