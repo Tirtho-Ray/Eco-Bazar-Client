72f834b978730ef07a8a78f03d2fceee57a57a34
Tirtho-Ray <tirthoray10@gmail.com>
2025-09-14 03:42:28 +0600

    update ui

--- src/pages/Home/StatsCards.jsx
+++ src/pages/Home/StatsCards.jsx
@@ -1,28 +1,28 @@
-import React from 'react';
-import { Container } from '../../components/ui/reUsable/Container';
+import React from "react";
+import { Container } from "../../components/ui/reUsable/Container";
 import bgImg from "../../assets/card/BG - Copy.png";
 
 const stats = [
-  { value: '37+', label: 'Years of Hard Work' },
-  { value: '500k+', label: 'Happy Customers' },
-  { value: '28', label: 'Qualified Team Members' },
-  { value: '750k+', label: 'Monthly Orders' },
+  { value: "37+", label: "Years of Hard Work" },
+  { value: "500k+", label: "Happy Customers" },
+  { value: "28", label: "Qualified Team Members" },
+  { value: "750k+", label: "Monthly Orders" },
 ];
 
 const StatsCards = () => {
   return (
     <div
-      className="py-10 bg-cover bg-center"
+      className="py-10 bg-cover bg-center mt-32"
       style={{
         backgroundImage: `url(${bgImg})`,
       }}
     >
       <Container>
-        <div className="flex flex-wrap justify-between gap-6">
+        <div className="flex flex-wrap justify-between gap-2">
           {stats.map((stat, index) => (
             <div
               key={index}
-              className="h-[174px] w-[312px] bg-white/5 backdrop-blur-md border border-white/20 rounded-xl shadow-lg flex flex-col items-center justify-center"
+              className="h-[174px] w-[300px] bg-white/5 backdrop-blur-md border border-white/20 rounded-xl shadow-lg flex flex-col items-center justify-center"
             >
               <span className="text-green-400 text-4xl font-semibold">
                 {stat.value}
